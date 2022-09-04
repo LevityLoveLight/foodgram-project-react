84d0483549825190266424342013b37deceaaff7
Fyodor Antonevich <Levity537@gmail.com>
2022-09-04 15:34:46 +0300

    creat model users

---
+++ backend/foodgram/users/models.py
@@ -0,0 +1,50 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+MAX_LENGTH = 64
+
+class User(AbstractUser):
+
+    login = models.CharField(
+        'Логин',
+         max_length=MAX_LENGTH,
+         blank=True,
+         unique=True
+         )
+    password = models.CharField(
+        'Пароль',
+        max_length = MAX_LENGTH,
+        blank=True,
+    )
+    email = models.EmailField(
+        'E-mail',
+        max_length=MAX_LENGTH,
+        blank=True,
+        unique=True
+        )
+    first_name = models.CharField(
+        'Имя',
+        max_length=MAX_LENGTH,
+        blank=True,
+        )
+    )
+    middle_name = models.CharField(
+        'Отчество',
+        max_length=MAX_LENGTH,
+        blank=False,
+        )
+    )
+    Last_name = models.CharField(
+        'Фамилия',
+        max_length=MAX_LENGTH,
+        blank=True,
+        )
+    )
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Пользователь'
+
+    def __str__(self):
+        return self.login
+
