f2bbaebe0850c7f29e6f6d917183c94b4e3ae3da
Fyodor Antonevich <Levity537@gmail.com>
2022-09-17 21:20:58 +0300

    v 1.0.10

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', default='foodgram'),
-        'USER': os.getenv('POSTGRES_USER', default='foodgram_user'),
+        'USER': os.getenv('POSTGRES_USER', default='admin'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='admin'),
         'HOST': os.getenv('DB_HOST', default='db'),
         'PORT': os.getenv('DB_PORT', default='5432'),
